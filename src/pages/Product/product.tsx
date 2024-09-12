ab22cef3c4838fb600d44d0e435c7729e3fa89fc
Nguyễn Văn Tuấn <tuansk1002@gmail.com>
2024-09-12 18:01:56 +0700

    update home page

--- src/pages/Product/product.tsx
+++ src/pages/Product/product.tsx
@@ -11,35 +11,35 @@ const product = () => {
       </div>
       <img className="top-0 w-full" src={mangreen}></img>
       <div className="hidden sm:grid grid-cols-4 h-[15%]">
-        <div className="bg-blend-saturation ">
+        <div className="bg-gradient-to-tl from-slate-200 to-slate-300">
           <img className="z-50" src={aovest}></img>
           <p className="text-left p-4 font-normal text-sm ">aovest PM</p>
         </div>
-        <div>
+        <div className="bg-gradient-to-tl from-slate-200 to-slate-300">
           <img src={aovest}></img>
           <p className="text-left p-4 font-normal text-sm">aovest PM</p>
         </div>
-        <div>
+        <div className="bg-gradient-to-tl from-slate-200 to-slate-300">
           <img src={aovest}></img>
           <p className="text-left p-4 font-normal text-sm">aovest PM</p>
         </div>
-        <div>
+        <div className="bg-gradient-to-tl from-slate-200 to-slate-300">
           <img src={aovest}></img>
           <p className="text-left p-4 font-normal text-sm">aovest PM</p>
         </div>
-        <div>
+        <div className="bg-gradient-to-tl from-slate-200 to-slate-300">
           <img src={aovest}></img>
           <p className="text-left p-4 gap-0 font-normal text-sm">aovest PM</p>
         </div>
-        <div>
+        <div className="bg-gradient-to-tl from-slate-200 to-slate-300">
           <img src={aovest}></img>
           <p className="text-left p-4 font-normal text-sm">aovest PM</p>
         </div>
-        <div>
+        <div className="bg-gradient-to-tl from-slate-200 to-slate-300">
           <img src={aovest}></img>
           <p className="text-left p-4 font-normal text-sm">aovest PM</p>
         </div>
-        <div>
+        <div className="bg-gradient-to-tl from-slate-200 to-slate-300">
           <img src={aovest}></img>
           <p className="text-left p-4 font-normal text-sm">aovest PM</p>
         </div>
